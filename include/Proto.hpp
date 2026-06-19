d9a21c40a87b845d2414a5db551c1800489a218b
77xt <77xt@proton.me>
2026-06-19 20:28:54 +0200

    no need to make CraftStream implement byte io interfaces

--- include/Proto.hpp
+++ include/Proto.hpp
@@ -11,7 +11,7 @@
 namespace vxc {
 
 template <typename IO>
-class CraftStream : public kinetic::BufReader, public kinetic::Writer {
+class CraftStream {
 private:
   std::shared_ptr<IO> _io;
 
@@ -25,14 +25,6 @@ public:
 
   ~CraftStream() = default;
 
-  kinetic::Result<usize> read(u8 * buf, usize size) override {
-    return _io->read(buf, size);
-  }
-
-  kinetic::Result<usize> write(const u8 * buf, usize size) override {
-    return _io->write(buf, size);
-  }
-
   kinetic::Result<i32> read_varint() {
     using ResultT = kinetic::Result<i32>;
 
@@ -41,7 +33,7 @@ public:
     u8 currentByte;
 
     while (true) {
-      const auto read_r = read_exact(&currentByte, 1);
+      const auto read_r = _io->read_exact(&currentByte, 1);
       if (read_r.is_err()) {
         return ResultT::err(read_r.get_error());
       }
@@ -68,7 +60,7 @@ public:
     u8 currentByte;
 
     while (true) {
-      const auto read_r = read_exact(&currentByte, 1);
+      const auto read_r = _io->read_exact(&currentByte, 1);
       if (read_r.is_err()) {
         return ResultT::err(read_r.get_error());
       }
@@ -91,11 +83,11 @@ public:
     while (true) {
       if ((value & ~SEGMENT_BITS) == 0) {
         const u8 value_byte = value;
-        return write_all(&value_byte, 1);
+        return _io->write_all(&value_byte, 1);
       }
 
       const u8 value_byte = (value & SEGMENT_BITS) | CONTINUE_BIT;
-      const auto res = write_all(&value_byte, 1);
+      const auto res = _io->write_all(&value_byte, 1);
       if (res.is_err()) {
         return res;
       }
@@ -108,117 +100,17 @@ public:
     while (true) {
       if ((value & ~((i64)SEGMENT_BITS)) == 0) {
         const u8 value_byte = value;
-        return write_all(&value_byte, 1);
+        return _io->write_all(&value_byte, 1);
       }
 
       const u8 value_byte = (value & SEGMENT_BITS) | CONTINUE_BIT;
-      const auto res = write_all(&value_byte, 1);
+      const auto res = _io->write_all(&value_byte, 1);
       if (res.is_err()) {
         return res;
       }
 
       value >>= 7;
     }
-  }
-
-  kinetic::Result<std::vector<rune>> read_string() noexcept {
-    using ResultT = kinetic::Result<std::vector<rune>>;
-
-    const auto ErrInvalidLeading      = ResultT::err(kinetic::ErrorKind::ValueInvalid, "invalid utf-8 leading byte");
-    const auto ErrInvalidContinuation = ResultT::err(kinetic::ErrorKind::ValueInvalid, "invalid utf-8 continuation byte");
-    const auto ErrInvalidRange        = ResultT::err(kinetic::ErrorKind::ValueInvalid, "invalid utf-8 range");
-
-    const auto len_r = read_varint();
-    if (len_r.is_err()) {
-      return ResultT::err(len_r.get_error());
-    }
-
-    const auto len = len_r.unwrap();
-
-    std::vector<rune> data;
-    data.reserve(len);
-
-    for (i32 i = 0; i < len; i++) {
-      u32 r = 0;
-      u8  c = 0;
-
-      const auto c_r = read_exact(&c, 1);
-      if (c_r.is_err()) {
-        return ResultT::err(c_r.get_error());
-      }
-
-      u8 b[3] = {0};
-
-      if      ((c & 0b11111000) == 0b11110000)
-      {
-        const auto b_r = read_exact(b, 3);
-        if (b_r.is_err()) return ResultT::err(b_r.get_error());
-
-        const u32 v_1 = c;
-        const u32 v_2 = b[0];
-        const u32 v_3 = b[1];
-        const u32 v_4 = b[2];
-
-        if ((v_2 & 0b11000000) != 0b10000000) return ErrInvalidContinuation;
-        if ((v_3 & 0b11000000) != 0b10000000) return ErrInvalidContinuation;
-        if ((v_4 & 0b11000000) != 0b10000000) return ErrInvalidContinuation;
-
-        r |= (v_1 & 0b00000111) << 18;
-        r |= (v_2 & 0b00111111) << 12;
-        r |= (v_3 & 0b00111111) << 06;
-        r |= (v_4 & 0b00111111) << 00;
-
-        if (r < 0x10000 || r > 0x10FFFF) return ErrInvalidRange;
-        if (r >= 0xD800 && r <= 0xDFFF)  return ErrInvalidRange;
-      }
-      else if ((c & 0b11110000) == 0b11100000)
-      {
-        const auto b_r = read_exact(b, 2);
-        if (b_r.is_err()) return ResultT::err(b_r.get_error());
-
-        const u32 v_1 = c;
-        const u32 v_2 = b[0];
-        const u32 v_3 = b[1];
-
-        if ((v_2 & 0b11000000) != 0b10000000) return ErrInvalidContinuation;
-        if ((v_3 & 0b11000000) != 0b10000000) return ErrInvalidContinuation;
-
-        r |= (v_1 & 0b00001111) << 12;
-        r |= (v_2 & 0b00111111) << 06;
-        r |= (v_3 & 0b00111111) << 00;
-
-        if (r < 0x800 || r > 0xFFFF)    return ErrInvalidRange;
-        if (r >= 0xD800 && r <= 0xDFFF) return ErrInvalidRange;
-      }
-      else if ((c & 0b11100000) == 0b11000000)
-      {
-        const auto b_r = read_exact(b, 1);
-        if (b_r.is_err()) return ResultT::err(b_r.get_error());
-
-        const u32 v_1 = c;
-        const u32 v_2 = b[0];
-
-        if ((v_2 & 0b11000000) != 0b10000000) return ErrInvalidContinuation;
-
-        r |= (v_1 & 0b00011111) << 06;
-        r |= (v_2 & 0b00111111) << 00;
-
-        if (r < 0x80 || r > 0x7FF)      return ErrInvalidRange;
-        if (r >= 0xD800 && r <= 0xDFFF) return ErrInvalidRange;
-      }
-      else if (c < 0b10000000)
-      {
-        r = c;
-      }
-      else
-      {
-        return ErrInvalidLeading;
-      }
-
-      data.emplace_back(rune(r));
-    }
-
-    return ResultT::ok(std::move(data));
   }
 
   kinetic::Result<u8> read_packet(Packet & packet) noexcept {
@@ -231,7 +123,7 @@ public:
     const auto p_len_v = p_len_r.unwrap();
 
     packet.set_len(usize(p_len_v));
-    const auto p_data_r = read_exact(packet.get_data().data(), p_len_v);
+    const auto p_data_r = _io->read_exact(packet.get_data().data(), p_len_v);
     if (p_data_r.is_err()) {
       return ResultT::err(p_data_r.get_error());
     }
@@ -249,7 +141,7 @@ public:
       return packet_len_r;
     }
 
-    const auto packet_body_r = write_all(packet.get_data().data(), packet_len);
+    const auto packet_body_r = _io->write_all(packet.get_data().data(), packet_len);
     if (packet_body_r.is_err()) {
       return packet_body_r;
     }
